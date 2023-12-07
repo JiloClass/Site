791bdd12f4613ce11fb1a8f2fb6a75a0a9bb8452
Nokaji <nokaji.dev@gmail.com>
2023-12-07 19:18:43 +0100

    update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -12,7 +12,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a @class(["nav-link"], "active" => Route::)
+                    <a @class(["nav-link"])
                     id="nav-link-home" href="index.php">Accueil</a>
                 </li>
                 <li class="nav-item">
